8cf796a44c79f89d77553be73476fc3ace98ddaa
douglas85rj <douglas85rj@gmail.com>
2023-05-19 11:04:04 -0300

    deploy

--- react-crud-front-main/src/services/axios.ts
+++ react-crud-front-main/src/services/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://fastify-crud-deploy-api.onrender.com'
+  //baseURL: 'https://fastify-crud-deploy-api.onrender.com'
+  baseURL: 'https://localhost:3333'
 });
